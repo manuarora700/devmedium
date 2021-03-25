7cf204c6e3690c2d0d8daeae3ae2ccc64779bff1
Manu Arora <mannuarora700@gmail.com>
2021-03-25 22:00:13 +0530

    ➕ ADD: Custom usernames in Firebase, 2-way mapping

--- pages/enter.js
+++ pages/enter.js
@@ -1,6 +1,8 @@
-import { auth, googleAuthProvider } from "../lib/firebase";
+import { auth, firestore, googleAuthProvider } from "../lib/firebase";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
+import { useState, useEffect, useCallback } from "react";
+import { debounce } from "lodash";
 
 export default function EnterPage({}) {
   const { user, username } = useContext(UserContext);
@@ -41,5 +43,114 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
-  return <div>username Form</div>;
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  const onChange = (e) => {
+    // Force form value typed in the form to match correct format
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // Only set form value if value is length is less than 3 OR it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both the documents.
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    // Commit both documents as a BATCH WRITE
+
+    const batch = firestore.batch();
+
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  // Hit the database for username match after each debounced change
+  // useCallback is required for DEBOUNCE - lodash package function to work
+
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log("Firestore read executed!");
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="username"
+            value={formValue}
+            onChange={onChange}
+          />
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>Debug State: </h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">{username} is taken!</p>;
+  } else {
+    return <p></p>;
+  }
 }

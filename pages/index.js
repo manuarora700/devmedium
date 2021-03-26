1dd8ef938930605a5f07a3203c6a6af5dcf1ffcc
Manu Arora <mannuarora700@gmail.com>
2021-03-26 13:26:46 +0530

    ➕ ADD: Homepage blogpost show data

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,74 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Loader from "../components/Loader";
 import toast from "react-hot-toast";
+import PostFeed from "../components/PostFeed";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
-export default function Home() {
+import { useState } from "react";
+
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      {/* <Link
-        prefetch={false}
-        href={{
-          pathname: "/[username]",
-          query: { username: "manuarora28" },
-        }}
-      >
-        <a>Manu's profile</a>
-      </Link> */}
-      {/* <Loader show /> */}
-      <button onClick={() => toast.success("Hello Toast")}>Toast</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button className="" onClick={getMorePosts}>
+          Load More
+        </button>
+      )}
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end"}
+    </main>
   );
 }
